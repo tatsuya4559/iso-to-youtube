070712a752eb537f269ce38a66095d36a811b917
tatsuya4559 <tatsuya.k1029@gmail.com>
2021-06-22 18:55:46 +0900

    Initial commit

---
+++ encode.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+INPUT=$1
+NAME=${INPUT#*iso/}
+NAME=${NAME%.*}
+OUTPUT=mp4/${NAME%.*}.mp4
+ffmpeg -i $INPUT $OUTPUT \
+  && ./upload_video.py --file=${OUTPUT} --title=${NAME} --privacyStatus="unlisted"
+  && echo DONE
